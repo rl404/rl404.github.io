dfed1c01ef50248b537f9d49bb00623cb3e229c0
Axel <Axel.rl.404@gmail.com>
2016-02-01 13:00:44 +0700

    Fix show name 2

--- osu/index.php
+++ osu/index.php
@@ -5,12 +5,12 @@
 	</head>
 	<body>
 		<h1>OSU!</h1>
-		<?php
+		<? php
+			echo 'asd';
 			$json_str = file_get_contents("osu.ppy.sh/api/get_user?k=270c5c3195f243df0696fde15864b12f56db030e&u=50265");
 			$data = json_decode($json_str,true);
 			$name = $data['user_id'];
-			echo $name;
-			echo 'asd';
+			echo $name;			
 		?>
 	</body>
 </html>
