a46ab6b7016c9e5662bcf8b8fa04d21b2b27ccca
axel <axel.rl.404@gmail.com>
2024-09-20 16:53:44 +0700

    feat: use simple task

--- src/routes/Experience.svelte
+++ src/routes/Experience.svelte
@@ -4,11 +4,19 @@
 	import LocationIcon from '$lib/components/icons/LocationIcon.svelte';
 	import UsersIcon from '$lib/components/icons/UsersIcon.svelte';
 	import { data } from '$lib/const';
+
+	let useSimple: boolean = false;
 </script>
 
-<div class="flex items-end gap-2">
-	<h2 class="text-3xl font-bold">Experience</h2>
-	<span class="hover">— {data.experiences.length.toLocaleString()}</span>
+<div class="flex items-center justify-between gap-2">
+	<div class="flex items-end gap-2">
+		<h2 class="text-3xl font-bold">Experience</h2>
+		<span class="hover">— {data.experiences.length.toLocaleString()}</span>
+	</div>
+	<div class="hover flex items-center gap-2">
+		<input id="useSimple" type="checkbox" bind:checked={useSimple} />
+		<label for="useSimple">Simpler Tasks</label>
+	</div>
 </div>
 
 <Border />
@@ -36,7 +44,7 @@
 			<div class="col-span-12 flex flex-col sm:col-span-9">
 				<h3 class="text-xl font-bold">{exp.position}</h3>
 				<ul class="list-disc pl-4">
-					{#each exp.tasks as w}
+					{#each useSimple ? exp.simpleTasks : exp.tasks as w}
 						<li>{w}</li>
 					{/each}
 				</ul>
